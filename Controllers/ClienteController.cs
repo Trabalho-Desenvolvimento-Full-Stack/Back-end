cbffc80931c3d623a4b914fdd8e582f4d1ad1e3c
John Duque <duquejohn185@gmail.com>
2021-12-02 20:41:16 -0300

    subindo context e controllers teste

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aereo.Context;
+using Ecommerce.Context;
+using Ecommerce.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (EcommerceContext ctx = new EcommerceContext())
+            {
+                return Ok(ctx.Clientes.ToList());
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetPeloId(int id)
+        {
+            using (EcommerceContext ctx = new EcommerceContext())
+            {
+                Cliente cliente = ctx.Clientes.Include(c=>c.Endereco).Where(c => c.Id.Equals(id)).FirstOrDefault();
+
+                if (cliente == null)
+                    return NotFound();
+
+                return Ok(cliente);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            using (EcommerceContext ctx = new EcommerceContext())
+            {
+                Cliente cliente = ctx.Clientes.Where(c => c.Id.Equals(id)).FirstOrDefault();
+
+                if (cliente == null)
+                    return NotFound();
+
+                ctx.Clientes.Remove(cliente);
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Post(Cliente cliente)
+        {
+            using (EcommerceContext ctx = new EcommerceContext())
+            {
+                ctx.Clientes.Add(cliente);
+                ctx.SaveChanges();
+            }
+            return Ok(cliente);
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login(Cliente cliente)
+        {
+            using (EcommerceContext ctx = new EcommerceContext())
+            {
+                Cliente usuario = ctx.Clientes.Where(u => u.Email.Equals(cliente.Email) && u.Senha.Equals(cliente.Senha)).FirstOrDefault();
+
+                if (usuario == null)
+                    return NotFound();
+                else
+                {
+                    usuario.Senha = "";
+                    return Ok(usuario);
+                }
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put(Cliente cliente)
+        {
+            using (EcommerceContext ctx = new EcommerceContext())
+            {
+                ctx.Clientes.Update(cliente);
+                ctx.SaveChanges();
+            }
+            return Ok(cliente);
+        }
+    }
+}
--- Entity/Cliente.cs
+++ Entity/Cliente.cs
@@ -1,6 +1,6 @@
 namespace Back_end.Entity
 {
-    public class Aluno{
+    public class Cliente{
     public int id {get; set;} 
     public string nome {get; set;}
     public string cpf {get; set;}
